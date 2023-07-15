b0c1ca32f98402af23221c46465e6bebaa826897
Cristina McClain <cristinarmcclain@gmail.com>
2023-07-15 14:32:06 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+class Program
+{
+static void Main(string[] args)
+    {
+        Console.WriteLine();
+    }
+}
+
+public class SQLiteDbContext
+{
+    public int CustomerID { get; set; }
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public string Address { get; set; }
+     public string City { get; set; }
+}
---
+++ knowledgecheck2.sql
@@ -0,0 +1,23 @@
+﻿-- Script Date: 7/14/2023 4:09 PM  - ErikEJ.SqlCeScripting version 3.5.2.94
+CREATE TABLE [Knowledgecheck5] (
+  [Id] INTEGER NOT NULL
+, CONSTRAINT [PK_KnowledgeCheck3] PRIMARY KEY ([Id])
+);
+CREATE DATABASE testDB;
+
+CREATE TABLE Customers (
+CustomerID int,
+LastName varchar(255),
+FirstName varchar(255),
+Address varchar(255),
+City varchar(255)
+);
+
+INSERT INTO Customers 
+VALUES ('77', 'Cheese', 'The Big', 'None', 'None')
+
+INSERT INTO Customers
+Values ('Product ID 40', 'Boston', 'Crab Meat', 'New England Seafood Cannery', 'Order Processing Dept. 2100 Paul Revere BLVD') 
+
+
+
