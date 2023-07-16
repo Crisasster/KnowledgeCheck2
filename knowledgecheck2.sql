6ff5555bf085c7f162e0aa6b89c6c41b3919c207
Cristina McClain <cristinarmcclain@gmail.com>
2023-07-16 10:28:17 -0400

    updated files

--- knowledgecheck2.sql
+++ knowledgecheck2.sql
@@ -1,23 +1,28 @@
 ﻿-- Script Date: 7/14/2023 4:09 PM  - ErikEJ.SqlCeScripting version 3.5.2.94
-CREATE TABLE [Knowledgecheck5] (
+CREATE TABLE [Knowledgecheck] (
   [Id] INTEGER NOT NULL
 , CONSTRAINT [PK_KnowledgeCheck3] PRIMARY KEY ([Id])
 );
 CREATE DATABASE testDB;
 
-CREATE TABLE Customers (
-CustomerID int,
-LastName varchar(255),
-FirstName varchar(255),
-Address varchar(255),
-City varchar(255)
+CREATE TABLE [dbo].[Product] (
+[CustomerID] varchar(255),
+[Name] varchar(255),
+[Address] varchar(255),
 );
 
-INSERT INTO Customers 
-VALUES ('77', 'Cheese', 'The Big', 'None', 'None')
+INSERT INTO CustomersID
+VALUES ('77', 'The Big Cheese')
+INSERT INTO CustomerID
+Values ('Product ID 40', 'Boston Crab Meat') 
 
-INSERT INTO Customers
-Values ('Product ID 40', 'Boston', 'Crab Meat', 'New England Seafood Cannery', 'Order Processing Dept. 2100 Paul Revere BLVD') 
+INSERT INTO Name
+VALUES ('The Big Cheese')
+INSERT INTO Name
+VALUES('New EngLand Seafood Cannery')
+
+INSERT INTO ADDRESS
+VALUES ('Order Processing Dept. 2100 Paul Revere BLVD')
 
 
 
